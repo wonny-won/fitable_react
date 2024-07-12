c4c4aa4294775ad3fa15b4ed065072b476822e7a
wonny0704 <jaake0704@ontune.co.kr>
2024-07-12 15:32:02 +0900

    설명 추가

--- src/commons/hooks/useChangeHandler.ts
+++ src/commons/hooks/useChangeHandler.ts
@@ -1,5 +1,19 @@
 import { ChangeEvent, useState } from "react"
 
+// 일반화 성공 함수 - 프로젝트 어느 input / textArea 에도 적용이 가능. 
+
+/** 
+ * input change 커스텀 훅스 핸들러 
+ * @returns inputs,onChangeHandler
+ * 
+ * [ 설 명 ]
+ *  1. inputs : 각 inputs / textArea의 state 
+ *  2. onChangeHandler : 각 inputs / textArea 의 상태값(state)을 끌어오는 onChange 함수
+ * 
+ * [ 참 고 ]
+ *  1. 각 input / textArea 컴포넌트에 porps로 id 값을 내려줘야 한다.
+ *  2. 각 id 값은 유니크해야 하며 스트링 값이다.
+ */
 export const useCustomChangeHandler = ()=>{
     const [inputs,setInpus] = useState({})
 
